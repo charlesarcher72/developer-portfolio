6e63ceeb04ec06fcbdbf05380616816737d2d15c
Charles Archer <88555441+charlesarcher72@users.noreply.github.com>
2024-01-04 13:37:25 -0500

    prevent pull to refresh

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 // IMPORTS
 import "./scss/App.scss";
-import React, { Component } from "react";
-import $                    from "jquery";
-import NavProvider          from './context/NavContext';
-import Nav                  from './components/Nav';
-import Header               from "./components/Header";
-import About                from "./components/About";
-import Experience           from "./components/Experience";
-import Projects             from "./components/Projects";
-import Skills               from "./components/Skills";
-import Services             from "./components/Services";
-import Footer               from "./components/Footer";
+import React, { Component, useEffect } from "react";
+import $ from "jquery";
+import NavProvider from './context/NavContext';
+import Nav from './components/Nav';
+import Header from "./components/Header";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 // APP
-class App extends Component 
-{
-  constructor(props) 
-  {
+class App extends Component {
+  constructor(props) {
     super();
     this.state = {
       resumeData: $.ajax({
@@ -34,20 +32,31 @@ class App extends Component
     };
   }
 
-  render() 
-  {
+  preventPullToRefresh = (e) => {
+    e.preventDefault();
+  };
+
+  componentDidMount() {
+    document.addEventListener('touchmove', this.preventPullToRefresh, { passive: false });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('touchmove', this.preventPullToRefresh);
+  }
+
+  render() {
     return (
-      // Componenets
-		  <NavProvider>
-				  <Nav                                           />
-          <Header sharedData={this.state.sharedData}     />
-          <About sharedData={this.state.sharedData}      />
-          <Experience sharedData={this.state.sharedData} />
-          <Projects sharedData={this.state.sharedData}   />
-          <Skills sharedData={this.state.sharedData}     />
-          <Services sharedData={this.state.sharedData}   />
-          <Footer sharedData={this.state.sharedData}     />
-			</NavProvider>
+      // Components
+      <NavProvider>
+        <Nav />
+        <Header sharedData={this.state.sharedData} />
+        <About sharedData={this.state.sharedData} />
+        <Experience sharedData={this.state.sharedData} />
+        <Projects sharedData={this.state.sharedData} />
+        <Skills sharedData={this.state.sharedData} />
+        <Services sharedData={this.state.sharedData} />
+        <Footer sharedData={this.state.sharedData} />
+      </NavProvider>
     );
   }
 }
