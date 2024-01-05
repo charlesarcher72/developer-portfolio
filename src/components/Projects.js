b563677a3f50916d7217303d472cd731fed1a210
Charles Archer <88555441+charlesarcher72@users.noreply.github.com>
2024-01-05 10:25:31 -0500

    responsive updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -74,7 +74,7 @@ class Projects extends Component {
           <h1 className="section-title">
             <span>{sectionName}</span>
           </h1>
-          <div className="col-md-6 mx-auto">
+          <div className="col-md-8 mx-auto">
             {projectsRows}
           </div>
           <ProjectModal
